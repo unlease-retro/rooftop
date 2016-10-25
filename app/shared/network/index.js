6f17fcfdbd7a8b75193f1802597d8e96de3e1ee2
Luke Hedger <luke.hedger@gmail.com>
2016-10-25 15:38:52 +0100

    add refresh token promise

--- app/shared/network/index.js
+++ app/shared/network/index.js
@@ -1,5 +1,6 @@
 import { RelayNetworkLayer, urlMiddleware, authMiddleware } from 'react-relay-network-layer'
 
+import { refreshToken } from '../../auth/actions'
 import { getAccessToken } from '../../auth/selectors'
 import { GRAPHQL_SERVER } from '../constants'
 
@@ -11,7 +12,10 @@ const configureNetwork = store => new RelayNetworkLayer([
 
   urlMiddleware({ url: () => GRAPHQL_SERVER }),
 
-  authMiddleware({ token: () => getAccessToken(store.getState()) }),
+  authMiddleware({
+    token: () => getAccessToken(store.getState()),
+    tokenRefreshPromise: () => store.dispatch(refreshToken()),
+  }),
 
   corsMiddleware({ credentials: 'same-origin' }),
 
