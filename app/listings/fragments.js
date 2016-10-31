ce64250f2b0bc262db1603c8d376e72aa232f473
Luke Hedger <luke.hedger@gmail.com>
2016-10-31 17:40:10 +0000

    add new fields/parms to listing

--- app/listings/fragments.js
+++ app/listings/fragments.js
@@ -2,9 +2,11 @@ import Relay from 'react-relay'
 
 export const query = () => Relay.QL`
   fragment on Query {
-    listings(lng: $lng, lat: $lat, radius: $radius) {
+    listings(lng: $lng, lat: $lat, radius: $radius, startDateInNextDays: $startDateInNextDays, listed: $listed) {
       id
       location
+      leakage
+      nonResponsive
       user {
         id
         firstName
--- app/listings/variables.js
+++ app/listings/variables.js
@@ -4,6 +4,8 @@ const variables = {
     lng: -0.12775829999998223,
     lat: 51.5073509,
     radius: 21,
+    startDateInNextDays: 90,
+    listed: true,
   },
 
   // TODO - `prepareVariables` could be used to intelligently apply filters -> like initialVariables.filter = London, lat: getLat(prevVariables.filter)
