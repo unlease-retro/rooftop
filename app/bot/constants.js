ed8084f20a97da4f65ceaa9f9ee282f4a957a260
Luke Hedger <luke.hedger@gmail.com>
2017-01-26 13:13:18 +0000

    add advert fields

--- app/bot/constants.js
+++ app/bot/constants.js
@@ -2,7 +2,39 @@ export const route = 'bot'
 
 export const FIELDS = [
   {
-    key : 'title',
+    key: 'title',
     label: 'Title',
+  },
+  {
+    key: 'name',
+    label: 'Name'
+  },
+  {
+    key: 'phoneNumber',
+    label: 'Phone'
+  },
+  {
+    key: 'description',
+    label: 'Description'
+  },
+  {
+    key: 'availabilityFrom',
+    label: 'Start Date'
+  },
+  {
+    key: 'availabilityTo',
+    label: 'End Date'
+  },
+  {
+    key: 'price',
+    label: 'Price'
+  },
+  {
+    key: 'city',
+    label: 'City'
+  },
+  {
+    key: 'postcode',
+    label: 'Postcode'
   },
 ]
