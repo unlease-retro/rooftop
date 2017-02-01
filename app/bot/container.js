9b30b1623aca3fa4429242ab5d2dfe98ded26567
Luke Hedger <luke.hedger@gmail.com>
2017-02-01 12:09:01 +0000

    add sorting logic to bot output

--- app/bot/container.js
+++ app/bot/container.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
 import Relay from 'react-relay'
-import { AutoSizer, Column, Table } from 'react-virtualized'
+import { AutoSizer, Column, SortIndicator, Table } from 'react-virtualized'
 
 import { FIELDS } from './constants'
 import * as fragments from './fragments'
 // import mutations from './mutations'
 import variables from './variables'
 // import { getAddressFromGeocode, getListingPreviewUrl, transformAdvertToListingPreview } from './util'
+import { getSortedList } from '../shared/util/virtualized'
 
 import { View } from 'components/layout'
 import { Text } from 'components/text'
 
 class Bot extends Component {
+
+  constructor() {
+
+    super()
+
+    this.onSort = this.onSort.bind(this)
+
+  }
+
+  onSort(variables) {
+
+    const { relay } = this.props
+
+    return relay.setVariables(variables)
+
+  }
 
   // onListingPreviewRequest(advert) {
   //
@@ -23,30 +40,40 @@ class Bot extends Component {
 
   render() {
 
-    const { allAdverts } = this.props.query
+    const { query, relay: { variables } } = this.props
+    const { allAdverts } = query
+    const { sortBy, sortDirection } = variables
 
-    const headerRenderer = ({ label }) => <div>{ label }</div>
+    // sort adverts
+    const sortedList = getSortedList(allAdverts, sortBy, sortDirection)
 
+    // make column headers sortable
+    const headerRenderer = ({ dataKey, label, sortBy, sortDirection }) => ( <div>{ label } { sortBy === dataKey && <SortIndicator sortDirection={ sortDirection } /> } </div> )
+
+    // create `Column` for each required field
     const renderColumns = FIELDS.map( (f, i) => <Column key={ i } headerRenderer={ headerRenderer } label={ f.label } dataKey={ f.key } width={1} flexGrow={1} flexShrink={0} /> )
 
     return (
       <View>
 
-        <Text>Bot</Text>
+        <Text atomic={{ fs:6, fw:'b', ta:'c' }} color='primary'>Bot Output</Text>
 
         <AutoSizer>
 
           { ({ width }) => (
 
             <Table
-              width={width}
+              width={ width }
               height={500}
               headerHeight={30}
               noRowsRenderer={ () => ( <div>No data</div> ) }
               overscanRowCount={10}
               rowCount={ allAdverts.length }
-              rowGetter={ ({ index }) => allAdverts[index] }
+              rowGetter={ ({ index }) => sortedList[index % sortedList.length] }
               rowHeight={40}
+              sort={ this.onSort }
+              sortBy={ sortBy }
+              sortDirection={ sortDirection }
               useDynamicRowHeight={false}
             >
               { renderColumns }
--- app/bot/variables.js
+++ app/bot/variables.js
@@ -3,6 +3,8 @@ const variables = {
   initialVariables: {
     submitted: false,
     disabled: false,
+    sortBy: 'title',
+    sortDirection: 'ASC'
   },
 
 }
