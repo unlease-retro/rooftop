ead5fa709563e403d51f824e269f77f56a2f53f7
Marius Balaj <marius@Mariuss-Mac-mini.local>
2017-02-06 11:02:53 +0200

    [ ADVERT ] - added ReduxForm element ✨

--- app/advert/container.js
+++ app/advert/container.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Relay from 'react-relay'
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form/immutable'
+import { Field } from 'redux-form/immutable'
 
 import * as Bot from '../bot'
 import { getStatus } from './computed'
@@ -9,14 +9,13 @@ import * as fragments from './fragments'
 import mutations from './mutations'
 import { mutations as ListingMutations } from '../listings'
 import variables from './variables'
-import { name } from './constants'
 import { getAddressFromGeocode, getListingPreviewUrl, transformAdvertToListing, transformAdvertToListingPreview } from './util'
 
 import { Anchor } from 'components/anchor'
 import { Button } from 'components/button'
 import { View } from 'components/layout'
 import { Text } from 'components/text'
-import { Form } from 'components/form'
+import { ReduxForm } from 'components/form'
 import { Label } from 'components/label'
 import { Input } from 'components/input'
 import { Textarea } from 'components/textarea'
@@ -67,50 +66,8 @@ class Advert extends Component {
     const onListingPreviewRequest = this.onListingPreviewRequest
     const onUpdateAdvertRequest = this.onUpdateAdvertRequest
 
-    const renderField = ({ input, defaultValue }) => <Input { ...input } value={ defaultValue }/>
-    const renderDescriptionField = ({ input, defaultValue }) => <Textarea { ...input } value={ defaultValue }/>
-
-    const Edit = reduxForm({ form: name })( () => (
-      <Form>
-
-        <Label>Title</Label>
-
-        <Field name='title' defaultValue={ advert.title } component={ renderField } />
-
-        <Label>Description</Label>
-
-        <Field name='description' type='text' defaultValue={ advert.description } component={ renderDescriptionField }/>
-
-        <Label>Price</Label>
-
-        <Field name='price' type='number' defaultValue={ advert.price } component={ renderField }/>
-
-        <Label>Host name</Label>
-
-        <Field name='hostName' type='text' defaultValue={ advert.hostName } component={ renderField }/>
-
-        <Label>Phone number</Label>
-
-        <Field name='phoneNumber' type='text' defaultValue={ advert.phoneNumber } component={ renderField }/>
-
-        <Label>Home type</Label>
-
-        <Field name='homeType' type='text' defaultValue={ advert.homeType } component={ renderField }/>
-
-        <Label>Location</Label>
-
-        <Field name='location' type='text' defaultValue={ advert.postcode } component={ renderField }/>
-
-        <Label>Availability from</Label>
-
-        <Field name='availabilityFrom' type='date' defaultValue={ advert.availabilityFrom } component={ renderField }/>
-
-        <Label>Availability to</Label>
-
-        <Field name='availabilityTo' type='date' defaultValue={ advert.availabilityTo } component={ renderField }/>
-
-      </Form>)
-    )
+    const FormInput = ({ input, defaultValue }) => <Input { ...input } value={ defaultValue }/>
+    const FormTextarea = ({ input, defaultValue }) => <Textarea { ...input } value={ defaultValue }/>
 
     // set computed values
     advert.status = getStatus(advert)
@@ -123,8 +80,37 @@ class Advert extends Component {
         <Text atomic={{ fs:3, mt:4, ta:'r' }}>Status: { advert.status }</Text>
 
         <Anchor atomic={{ d:'b', mb:4, td:'n' }} to={Bot.route}>&larr; Back</Anchor>
+        
+        <ReduxForm>
 
-        <Edit/>
+          <Label>Title</Label>
+          <Field name='title' defaultValue={ advert.title } component={ FormInput } />
+
+          <Label>Description</Label>
+          <Field name='description' type='text' defaultValue={ advert.description } component={ FormTextarea }/>
+
+          <Label>Price</Label>
+          <Field name='price' type='number' defaultValue={ advert.price } component={ FormInput }/>
+
+          <Label>Host name</Label>
+          <Field name='hostName' type='text' defaultValue={ advert.hostName } component={ FormInput }/>
+
+          <Label>Phone number</Label>
+          <Field name='phoneNumber' type='text' defaultValue={ advert.phoneNumber } component={ FormInput }/>
+
+          <Label>Home type</Label>
+          <Field name='homeType' type='text' defaultValue={ advert.homeType } component={ FormInput }/>
+
+          <Label>Location</Label>
+          <Field name='location' type='text' defaultValue={ advert.postcode } component={ FormInput }/>
+
+          <Label>Availability from</Label>
+          <Field name='availabilityFrom' type='date' defaultValue={ advert.availabilityFrom } component={ FormInput }/>
+
+          <Label>Availability to</Label>
+          <Field name='availabilityTo' type='date' defaultValue={ advert.availabilityTo } component={ FormInput }/>
+
+        </ReduxForm>
 
         <View atomic={{ ta: 'c' }}>
 
--- app/shared/components/form.js
+++ app/shared/components/form.js
@@ -1,7 +1,9 @@
+import { reduxForm } from 'redux-form/immutable'
 import styled from 'styled-components'
 import Atomic from 'style/atomic'
 
 import { colours, typography } from 'style'
+import { name as form } from '../../advert/constants'
 
 export const Form = styled.form`
   ${ typography.ff() }
@@ -10,6 +12,8 @@ export const Form = styled.form`
   color: ${ colours.dark };
   ${ ({ atomic }) => Atomic({ ...Form.default.atomic, ...atomic }) }
 `
+
+export const ReduxForm = reduxForm( { form } )( Form )
 
 Form.default = {
   atomic: {
